ebdb533f74e698ca91e0cff6d7d2733d4a403702
Xiaodong Li <2238033733@qq.com>
2021-12-02 16:20:47 +0800

    update: component.ts

--- src/template/component.ts
+++ src/template/component.ts
@@ -1,4 +1,5 @@
 import * as ts from "typescript";
+import {ConstKeyword, ExclamationToken, ModifierFlags, NodeFlags} from "typescript";
 
 export type RouteType = {
     path: string;
@@ -9,31 +10,36 @@ export type RouterType = RouteType[]
 export const renderComponent = (name: string) => {
     function makeReactAppFunction() {
         const statement = ts.factory.createImportDeclaration(
-            /*decorators*/ undefined,
-            /*modifiers*/ undefined,
-            ts.factory.createImportClause(false, ts.factory.createIdentifier('React'), /*namedBindings*/ undefined),
+            undefined,
+            undefined,
+            ts.factory.createImportClause(false, ts.factory.createIdentifier('React'), undefined),
             ts.factory.createStringLiteral('react'))
 
-        const functionName = ts.factory.createIdentifier("App")
-        const openingElement = ts.factory.createJsxOpeningElement(ts.factory.createIdentifier("Router"), [], ts.factory.createJsxAttributes([]))
-        const closingElement = ts.factory.createJsxClosingElement(ts.factory.createIdentifier("Router"))
+        const div = ts.factory.createIdentifier("div")
+        const openingElement = ts.factory.createJsxOpeningElement(div, [], ts.factory.createJsxAttributes([]))
+        const closingElement = ts.factory.createJsxClosingElement(div)
         const jsxElement = ts.factory.createJsxElement(openingElement, [], closingElement)
         const returnStatement = [ts.factory.createReturnStatement(jsxElement)]
-        const makeFunction = ts.factory.createFunctionExpression(
-            /*modifiers*/ undefined,
-            /*asteriskToken*/ undefined,
-            functionName,
-            /*typeParameters*/ undefined,
-            /*parameters*/ undefined,
-            /*returnType*/ undefined,
-            ts.factory.createBlock(returnStatement, /*multiline*/ true))
-        return makeFunction
+        const makeFunction = ts.factory.createArrowFunction(
+            undefined,
+            undefined,
+            [],
+            undefined,
+            undefined,
+            ts.factory.createBlock(returnStatement, true))
+        const varStatement = ts.factory.createVariableDeclaration(name, undefined, ts.factory.createKeywordTypeNode(ts.SyntaxKind.NumberKeyword), makeFunction)
+        const result = ts.factory.createVariableStatement(
+            [ts.factory.createModifier(ts.SyntaxKind.ConstKeyword)],
+            [varStatement]
+        )
+        return result
         //  type: TypeNode | undefined, equalsGreaterThanToken: EqualsGreaterThanToken | undefined, body: ConciseBody): ArrowFunction;
     }
 
     const printer = ts.createPrinter({newLine: ts.NewLineKind.LineFeed})
     const sourceFile = ts.createSourceFile("sourceFileName.ts", "", ts.ScriptTarget.Latest, false, ts.ScriptKind.TS)
     const result = printer.printNode(ts.EmitHint.Unspecified, makeReactAppFunction(), sourceFile)
+    console.log(result)
     return result
 }
 
