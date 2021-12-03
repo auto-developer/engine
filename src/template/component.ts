aed4c9217dbac274d72424c8301ab9dc70a5bd3a
lossa <luomusha@gmail.com>
2021-12-03 13:08:01 +0800

    init jest

--- src/template/component.ts
+++ src/template/component.ts
@@ -1,55 +1,86 @@
-import * as ts from "typescript";
-import {ConstKeyword, ExclamationToken, Expression, ModifierFlags, NodeFlags} from "typescript";
+import {
+    createPrinter,
+    createSourceFile, EmitHint,
+    factory,
+    NewLineKind,
+    ReturnStatement,
+    ScriptKind,
+    ScriptTarget,
+    SyntaxKind
+} from "typescript";
 
 export type RouteType = {
     path: string;
     component: string;
 }
-export type RouterType = RouteType[]
+
+const importTemplate = () => {
+    const statement = factory.createImportDeclaration(
+        undefined,
+        undefined,
+        factory.createImportClause(false, factory.createIdentifier('React'), undefined),
+        factory.createStringLiteral('react')
+    )
+    return statement
+}
+
+const componentTemplate = (): ReturnStatement => {
+    const div = factory.createIdentifier("div")
+    const openingElement = factory.createJsxOpeningElement(div, [], factory.createJsxAttributes([]))
+    const closingElement = factory.createJsxClosingElement(div)
+    const jsxElement = factory.createJsxElement(openingElement, [], closingElement)
+    return factory.createReturnStatement(jsxElement)
+}
+
+const makeReactAppFunction = (name: string) => {
+
+    const paramName = factory.createIdentifier("props")
+    const parameter = factory.createParameterDeclaration(
+        undefined,
+        undefined,
+        undefined,
+        paramName
+    )
+    const makeArrowFunction = factory.createArrowFunction(
+        undefined,
+        undefined,
+        [parameter],
+        undefined,
+        undefined,
+        factory.createBlock([componentTemplate()], true))
+    const varStatement = factory.createVariableDeclaration(name,
+        undefined,
+        factory.createLiteralTypeNode(factory.createRegularExpressionLiteral('FC<PropsType>')),
+        makeArrowFunction)
+    const result = factory.createVariableStatement(
+        [factory.createModifier(SyntaxKind.ConstKeyword)],
+        [varStatement]
+    )
+    return result
+}
+
+const exportTemplate = () => {
+    const statement = factory.createExportDeclaration(
+        [],
+        undefined,
+        false,
+        undefined,
+        )
+    return statement
+}
+
+const template = () => {
+    return factory.createBlock([
+        importTemplate(),
+        makeReactAppFunction("App"),
+        exportTemplate()
+    ], true)
+}
 
 export const renderComponent = (name: string) => {
-    function makeReactAppFunction() {
-        const statement = ts.factory.createImportDeclaration(
-            undefined,
-            undefined,
-            ts.factory.createImportClause(false, ts.factory.createIdentifier('React'), undefined),
-            ts.factory.createStringLiteral('react'))
-
-        const paramName = ts.factory.createIdentifier("props")
-        const parameter = ts.factory.createParameterDeclaration(
-            undefined,
-            undefined,
-            undefined,
-            paramName
-        )
-        /* return body */
-        const div = ts.factory.createIdentifier("div")
-        const openingElement = ts.factory.createJsxOpeningElement(div, [], ts.factory.createJsxAttributes([]))
-        const closingElement = ts.factory.createJsxClosingElement(div)
-        const jsxElement = ts.factory.createJsxElement(openingElement, [], closingElement)
-        const returnStatement = [ts.factory.createReturnStatement(jsxElement)]
-
-        const makeArrowFunction = ts.factory.createArrowFunction(
-            undefined,
-            undefined,
-            [parameter],
-            undefined,
-            undefined,
-            ts.factory.createBlock(returnStatement, true))
-        const varStatement = ts.factory.createVariableDeclaration(name,
-            undefined,
-            ts.factory.createLiteralTypeNode(ts.factory.createRegularExpressionLiteral('FC<PropsType>')),
-            makeArrowFunction)
-        const result = ts.factory.createVariableStatement(
-            [ts.factory.createModifier(ts.SyntaxKind.ConstKeyword)],
-            [varStatement]
-        )
-        return result
-    }
-
-    const printer = ts.createPrinter({newLine: ts.NewLineKind.LineFeed})
-    const sourceFile = ts.createSourceFile("sourceFileName.ts", "", ts.ScriptTarget.Latest, false, ts.ScriptKind.TS)
-    const result = printer.printNode(ts.EmitHint.Unspecified, makeReactAppFunction(), sourceFile)
+    const printer = createPrinter({newLine: NewLineKind.LineFeed})
+    const sourceFile = createSourceFile("sourceFileName.ts", "", ScriptTarget.Latest, false, ScriptKind.TS)
+    const result = printer.printNode(EmitHint.Unspecified, template(), sourceFile)
     return result
 }
 
